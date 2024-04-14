2510ff4184e892095a97c641a3ce5a456d91ed81
MaikaSaba <saba.maika@gmail.com>
2024-04-13 22:58:24 -0300

    create connection with db, not working yet

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+// Include packages needed for this application
+const mysql = require('mysql2');
+
+// our DB config --> HOW we connect (and to w0aht DB)
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '12345678',
+  database: 'business_db'
+});
+
+// this is the connection 
+connection.connect(function(error) {
+    if(error) {
+        console.log("Error: ", error);
+        throw error
+    }
+    // console.log("DB connected...");
+})
+
+module.exports = connection;
---
+++ db/schema.js
@@ -0,0 +1,40 @@
+-- Drop the existing database if it exists
+DROP DATABASE business_db;
+
+-- Create a new database
+CREATE DATABASE business_db;
+
+-- Use the newly created database
+USE business_db;
+
+-- Create a 'departments' table
+CREATE TABLE departments (
+    departmentId INT AUTO_INCREMENT PRIMARY KEY,
+    departmentName VARCHAR(30) NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Create a 'roles' table
+CREATE TABLE roles (
+    roleId INT AUTO_INCREMENT PRIMARY KEY,
+    roleTitle VARCHAR(30) NOT NULL,
+    roleSalary DECIMAL(10, 2) NOT NULL,
+    departmentId INT NOT NULL,
+    FOREIGN KEY (departmentId) REFERENCES departments(departmentId),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Create an 'employees' table
+CREATE TABLE employees (
+    employeeId INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    roleId INT NOT NULL,
+    managerId INT,
+    FOREIGN KEY (roleId) REFERENCES roles(roleId),
+    FOREIGN KEY (managerId) REFERENCES employees(employeeId),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
